186fdb9b12343f50aabb4419aa3b7b17fa539adf
Nathan <nathaniwilliamsuk@gmail.com>
2020-12-11 08:59:19 +0000

    remove code duplication

--- 11/11.py
+++ 11/11.py
@@ -23,43 +23,6 @@ def part1Helper(x,y,s,r,l):
 
     return neighbors
 
-def part1Round(l):
-    m = []
-    changes = 0
-    for x,r in enumerate(l):
-        mr = ''
-        for y ,s in enumerate(r):
-            if y == '.':
-                mr = mr + '.'
-            else:
-                neighbors = part1Helper(x,y,s,r,l)
-                #print(x,y,s,neighbors)
-                if s == 'L' and neighbors == 0:
-                    changes = changes +1
-                    mr = mr + '#'
-                elif s == '#' and neighbors >=4:
-                    changes = changes +1
-                    mr = mr + 'L'
-                else:
-                    mr = mr + s
-        m.append(mr)
-    return m, changes == 0
-
-def part1(l):
-    changes = False
-    while not changes:
-        m, changes = part1Round(l)
-        #print(changes)
-        l = m
-    #prettyPrint(l)
-
-    count = 0
-    for r in l:
-        for s in r:
-            if s == '#':
-                count = count +1
-    print(count)
-
 def part2Helper(x,y,s,r,l):
     neighbor = [(-1,-1),(-1,0),(-1,1),
                 (0,-1),          (0,1),
@@ -88,7 +51,7 @@ def part2Helper(x,y,s,r,l):
 
 
 
-def part2Round(l):
+def round(part,l):
     m = []
     changes = 0
     for x,r in enumerate(l):
@@ -97,12 +60,17 @@ def part2Round(l):
             if y == '.':
                 mr = mr + '.'
             else:
-                neighbors = + part2Helper(x,y,s,r,l)
+                if part ==2:
+                    neighborLimit = 5
+                    neighbors = + part2Helper(x,y,s,r,l)
+                else:
+                    neighbors = + part1Helper(x,y,s,r,l)
+                    neighborLimit = 4
                 #print(x,y,s,neighbors)
                 if s == 'L' and neighbors == 0:
                     changes = changes +1
                     mr = mr + '#'
-                elif s == '#' and neighbors >=5:
+                elif s == '#' and neighbors >= neighborLimit:
                     changes = changes +1
                     mr = mr + 'L'
                 else:
@@ -110,10 +78,10 @@ def part2Round(l):
         m.append(mr)
     return m, changes == 0
 
-def part2(l):
+def part(part,l):
     changes = False
     while not changes:
-        m, changes = part2Round(l)
+        m, changes = round(part,l)
         l = m
     #prettyPrint(l)
 
@@ -132,5 +100,5 @@ l = []
 for x in f:
     l.append(x[:-1])
 
-part1(l)
-part2(l)
+part(1,l)
+part(2,l)
